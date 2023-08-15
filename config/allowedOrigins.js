b98b6736cee98104e04b1484007072a9772eaee0
Claudia Belanger <claudiabelanger@Claudias-iMac.local>
2023-08-15 17:31:21 -0400

    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,10 @@
 const allowedOrigins = [
-    'https://mushy-deer-leather-jacket.cyclic.cloud',
-    'https://www.app.agence-aloha.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3050',
-    'http://localhost:4000'
+    '*'
+    // 'https://mushy-deer-leather-jacket.cyclic.cloud',
+    // 'https://www.app.agence-aloha.com',
+    // 'http://127.0.0.1:5500',
+    // 'http://localhost:3050',
+    // 'http://localhost:4000'
 ];
 
 module.exports = allowedOrigins;
